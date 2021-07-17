8fb2a53a13285c1fa0b35a177acadc97e12148f7
Luis Duque <33474682+luismiguelduque@users.noreply.github.com>
2021-07-17 11:31:00 -0400

    ajustes faq

--- components/faq-section.js
+++ components/faq-section.js
@@ -2,6 +2,10 @@ const HeaderSection = Vue.component('header-section', {
     template: `
         <div>
             FAQ section
+            <div v-for="item in items">
+                <div>{{ item.question }}</div>
+                <div>{{ item.answer }}</div>
+            </div>
         </div>
     `,
     props: {
